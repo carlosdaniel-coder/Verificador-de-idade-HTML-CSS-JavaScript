ebea49c0d151f64d407fee76c2c0911216cfdddd
Carlos Daniel <136939779+carlosdaniel-coder@users.noreply.github.com>
2023-07-31 19:50:16 -0300

    upgrade

--- site/js/script.js
+++ site/js/script.js
@@ -1,82 +1,61 @@
-var agora = new Date()
-var ano = agora.getFullYear()
-
-var input = window.document.getElementsByClassName('ano')[0]
-
-var txt = window.document.querySelector('p.txt')
-
-var foto = window.document.getElementsByClassName('foto')[0]
-
-var masculino = document.getElementById('mas')
-var feminino = document.getElementById('fem')
-
-var sexo = 0
-
-var nome = ''
-
-function clicar(a) {
-    if (a == 1) {
-        masculino.checked = true
-        feminino.checked = false
-        return sexo = a
-    } else {
-        masculino.checked = false
-        feminino.checked = true
-        return sexo = a
-    }
-
-}
-
 function verificar() {
-    var idade = ano - input.value // idade 
-    if (idade == 0 || idade == 1) { // bebê
-        nome = 'Bebê'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/01.jpg)'
-        } else {
-            foto.style.backgroundImage = 'url(imagens/02.jpg)'
-        }
-    } else if (idade < 12) { // criança
-        nome = 'Criança'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/11.jpg)'
-        } else {
-            foto.style.backgroundImage = 'url(imagens/12.jpg)'
-        }
-    } else if (idade < 24) { // adolescente
-        nome = 'Adolescente'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/21.jpg)'
-        } else {
-            foto.style.backgroundImage = 'url(imagens/22.jpg)'
-        }
-    } else if (idade < 40) { // Adulto
-        nome = 'Adulto'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/31.jpg)'
-        } else {
-            foto.style.backgroundImage = 'url(imagens/32.jpg)'
-        }
-    } else if (idade < 59) { // Coroa
-        nome = 'Coroa'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/41.jpg)'
-        } else {
-            foto.style.backgroundImage = 'url(imagens/42.jpg)'
-        }
-    } else if (idade < 122) { // Idoso
-        nome = 'Idoso'
-        if (sexo == 1) {
-            foto.style.backgroundImage = 'url(imagens/51.jpg)'
-        } else {
-            nome = 'Idosa'
-            foto.style.backgroundImage = 'url(imagens/52.jpg)'
-        }
-    } if (idade < 0 || idade > 122) { // meme
+    var data = new Date()
+    var agoraAno = data.getFullYear()
+    var fano = window.document.getElementById('nascimento')
+    var res = document.getElementById('res')
+    if (fano.value <= 0 || Number(fano.value) >= agoraAno) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-        foto.style.backgroundImage = 'url(imagens/burro.jpg)'
-        txt.innerHTML = `Tá doido fí skskks`
     } else {
-        txt.innerHTML = `Detectamos ${nome} com ${idade} anos.`
+        var fsex = document.getElementsByName('radsex')
+        var idade = agoraAno - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade <= 2) {
+                // bebe
+                img.setAttribute('src', 'imagens/bebeM.png')
+            } else if (idade <= 14) {
+                // criança
+                img.setAttribute('src', 'imagens/criançaM.png')
+            } else if (idade <= 18) {
+                // adolescente
+                img.setAttribute('src', 'imagens/adolescenteM.png')
+            } else if (idade <= 40) {
+                // adulto
+                img.setAttribute('src', 'imagens/adultoM.png')
+            } else if (idade <= 60) {
+                // coroa
+                img.setAttribute('src', 'imagens/coroaM.png')
+            } else {
+                // velho
+                img.setAttribute('src', 'imagens/velhoM.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade <= 2) {
+                // bebe
+                img.setAttribute('src', 'imagens/bebeF.png')
+            } else if (idade <= 12) {
+                // criança
+                img.setAttribute('src', 'imagens/criançaF.png')
+            } else if (idade <= 18) {
+                // adolescente
+                img.setAttribute('src', 'imagens/adolescenteF.png')
+            } else if (idade <= 40) {
+                // adulto
+                img.setAttribute('src', 'imagens/adultoF.png')
+            } else if (idade <= 60) {
+                // coroa
+                img.setAttribute('src', 'imagens/coroaF.png')
+            } else {
+                // velho
+                img.setAttribute('src', 'imagens/velhoF.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
